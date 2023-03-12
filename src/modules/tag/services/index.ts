ae8065389b971ce718d6f89a6cff95a6d0dc2641
Huy Van <vamictk62@gmail.com>
2023-03-12 23:36:47 +0700

    Push

--- src/modules/tag/services/index.ts
+++
@@ -1,8 +0,0 @@
-import { TagType } from '../../../types/type.tag';
-import tagDaos from '../daos';
-
-const create = async (tagDTO: TagType) => {
-  return await tagDaos.createTag(tagDTO);
-};
-
-export default { create };
